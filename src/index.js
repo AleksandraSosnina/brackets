51d0c6dce3a7331a4a3a02bd0c5d195744e46955
Sosnina Sasha <aleksandra.sosnina@unitbean.com>
2020-10-15 15:50:09 +0400

    feat: breackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const numberOrCharRegexp = /[0-9]|[a-z]/i;
+  let regex = "";
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const currentBrackets = bracketsConfig[i];  // = ['(', ')']
+
+    let currentRegex = '';
+
+    if (numberOrCharRegexp.test(currentBrackets[0])) {
+      currentRegex = currentBrackets[0] + currentBrackets[1];
+    } else {
+      currentRegex = '\\' + currentBrackets[0] + '\\' + currentBrackets[1];
+    }
+    if (i == 0) {
+      regex += currentRegex;
+    } else {
+      regex += '|' + currentRegex;
+    }
+  }
+
+  const brackets = new RegExp(regex, 'i');
+  
+  while (brackets.test(str)) {
+    str = str.replace(brackets, '');
+  }
+
+  if (str.length == 0) {
+    return true;
+  }
+   else {
+     return false;
+   }
+
 }
+
+module.exports = check; 
